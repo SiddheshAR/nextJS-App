9473ed7ed2b1b85744868f80f8e06786d333e905
CDCSiddheshAcharekar <siddhesh@curiousdevelopers.in>
2024-04-21 23:36:45 +0530

    Tut2

---
+++ src/app/blog/[slug]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div>Blog Zone</div>
+  )
+}
+
+export default BlogPage
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+
+import Links from './links.jsx';
+
+const Navbar = () => {
+  return (
+    <div>
+        <h2>Navbar</h2>
+        <Links/>
+    </div>
+  )
+}
+
+export default Navbar
